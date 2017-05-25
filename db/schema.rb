e1b11dcca009d667e183b27b8850cdbaf7ab9748
matikamien_93@hotmail.com <matikamien_93@hotmail.com>
2017-05-25 20:06:53 -0300

    Adds name and surname to users. Create relation between users and events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525224945) do
+ActiveRecord::Schema.define(version: 20170525230608) do
+
+  create_table "Events_Users", id: false, force: :cascade do |t|
+    t.integer "user_id",  limit: 4, null: false
+    t.integer "event_id", limit: 4, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -33,6 +38,8 @@ ActiveRecord::Schema.define(version: 20170525224945) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 255
+    t.string   "surname",                limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
