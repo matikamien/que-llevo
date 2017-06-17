6d36e49aff115b806b370d81386581fdd3c02fbd
matikamien_93@hotmail.com <matikamien_93@hotmail.com>
2017-06-17 20:21:39 -0300

    Includes devise modules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,10 @@ class ApplicationController < RocketPants::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
-  include ActionController::MimeResponds
   include Devise::Controllers::Helpers
   include Doorkeeper::Rails::Helpers
+  include ActionController::MimeResponds
+  include ActionController::ImplicitRender
   
   def current_user
     @current_user ||= User.find doorkeeper_token.resource_owner_id
