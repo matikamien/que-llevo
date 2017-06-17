bc0ea101ed2269cac030a5187c291c9fdf2e30df
matikamien_93@hotmail.com <matikamien_93@hotmail.com>
2017-06-17 20:16:13 -0300

    Add MimeResponds to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < RocketPants::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
+  include ActionController::MimeResponds
   include Devise::Controllers::Helpers
   include Doorkeeper::Rails::Helpers
   
