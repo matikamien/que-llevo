304ae52c33c34335621721ecc1674c0f74e0de0a
matikamien_93@hotmail.com <matikamien_93@hotmail.com>
2017-07-02 01:31:31 -0300

    Se modifica el event user serializer, dejando de serializar al User.

--- app/serializers/event_user_serializer.rb
+++ app/serializers/event_user_serializer.rb
@@ -1,6 +1,13 @@
 class EventUserSerializer < ActiveModel::Serializer
- 
-  has_one :user, serializer: UserShortSerializer
+  attributes :id, :name, :surname
   has_many :event_items, serializer: EventItemSerializer
+
+  def name
+  	(User.find object.user_id).name
+  end
+
+  def surname
+  	(User.find object.user_id).surname
+  end
 
 end
